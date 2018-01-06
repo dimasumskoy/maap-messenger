da8bbadb324cae05b783c6f2c20eae603936c116
Dima <sumskoydima@gmail.com>
2018-01-06 13:41:09 +0300

    Build model associations with specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  has_many :conversations, foreign_key: :sender_id
+  has_many :messages
+
   validates :name, presence: true
 end
--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Conversation, type: :model do
   it { should belong_to :sender }
   it { should belong_to :recipient }
+  it { should have_many :messages }
 end
