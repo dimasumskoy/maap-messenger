9518ad671d84807d931fe04700f2c7faf8cbe33b
Dima <sumskoydima@gmail.com>
2018-01-06 13:41:09 +0300

    Build model associations with specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many :conversations }
+  it { should have_many :messages }
   it { should validate_presence_of :name }
 end
