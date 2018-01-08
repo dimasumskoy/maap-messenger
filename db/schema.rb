6d192194eb339aa20a8c64cc8a2ca7a08ea20ec7
Dima <sumskoydima@gmail.com>
2018-01-08 12:09:03 +0300

    Reinstall database

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20180105122720) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.integer "conversations_amount"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
