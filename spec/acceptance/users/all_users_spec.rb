48f45da749b3f9fca06c9cbe90308caa1fb35bcb
Dima <sumskoydima@gmail.com>
2018-01-09 21:25:59 +0300

    Add Users list with specs

---
+++ spec/acceptance/users/all_users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'List of users', %q{
+  In order to start conversation
+  As a user
+  I want to be able to see the list of all users
+} do
+  given!(:users) { create_list(:user, 5) }
+
+  scenario 'User can see the list of other users' do
+    visit users_path
+
+    users.each do |user|
+      expect(page).to have_content user.name
+      expect(page).to have_content user.username
+    end
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:name)     { |n| "user#{n}" }
+    sequence(:username) { |n| "@user#{n}" }
+    admin false
   end
 end
