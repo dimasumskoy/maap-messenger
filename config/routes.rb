8b5519ad99abf1845ad56cc24758789be0ae2346
Dima <sumskoydima@gmail.com>
2018-01-18 21:23:27 +0300

    Configure sign_up process

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '/', path_names: {
-    registration: 'sign_up',
-    sign_up: '/',
-    sign_in: 'login',
-    sign_out: 'logout'
-  }
+  devise_for :users,
+    controllers: { registrations: 'users/registrations' },
+    path: '/',
+    path_names: {
+      registration: 'sign_up',
+      sign_up:      '/',
+      sign_in:      'login',
+      sign_out:     'logout'
+    }
 
   resources :users
   resources :conversations
