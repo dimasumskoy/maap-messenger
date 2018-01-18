caa56b8918f7330f370ef754e65dc58cd61cccce
Dima <sumskoydima@gmail.com>
2018-01-18 22:48:55 +0300

    Add conversations#show with view

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,25 @@
 class ConversationsController < ApplicationController
+  before_action :set_conversation, only: [:show]
+
   def index
     @conversations = current_user.conversations
+  end
+
+  def show
+    if @conversation.present?
+      @sender_name    = @conversation.sender_name
+      @recipient_name = @conversation.recipient_name
+    else
+      create
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def set_conversation
+    @conversation = Conversation.find(params[:id])
   end
 end
