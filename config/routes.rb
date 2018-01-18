abbef97ad865f48445dab908535ad7c1feeb68b8
Dima <sumskoydima@gmail.com>
2018-01-18 20:42:13 +0300

    Setup devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '/', path_names: {
+    registration: 'sign_up',
+    sign_up: '/',
+    sign_in: 'login',
+    sign_out: 'logout'
+  }
+
   resources :users
   resources :conversations
 
