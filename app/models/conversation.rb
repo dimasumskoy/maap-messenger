467b66739986f302ab54a1772006a0bdd63bb84d
Dima <sumskoydima@gmail.com>
2018-01-20 00:40:53 +0300

    Implement messaging within two users (raw)

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,4 +3,22 @@ class Conversation < ApplicationRecord
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
 
   has_many :messages
+
+  def self.get(sender_name, recipient_name)
+    where(
+      sender_name: sender_name,
+      recipient_name: recipient_name
+    ).or(
+      where(sender_name: recipient_name,
+            recipient_name: sender_name)
+    ).first
+  end
+
+  def self.for_user(user)
+    where(sender: user).or(where(recipient: user))
+  end
+
+  def opposed(user)
+    sender == user ? recipient : sender
+  end
 end
