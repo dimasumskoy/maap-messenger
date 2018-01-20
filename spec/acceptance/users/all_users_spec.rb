6af5f69f7561bcbd4ef04412d18affb724f68cc6
Dima <sumskoydima@gmail.com>
2018-01-21 01:08:38 +0300

    Add model specs

--- spec/acceptance/users/all_users_spec.rb
+++ spec/acceptance/users/all_users_spec.rb
@@ -1,18 +1,21 @@
-require 'rails_helper'
+require 'acceptance/acceptance_helper'
 
 feature 'list of users', %q{
   In order to start conversation
   As a user
   I want to be able to see the list of all users
 } do
+
+  given(:user) { create(:user) }
   given!(:users) { create_list(:user, 5) }
 
   scenario 'User can see the list of other users' do
+    sign_in(user)
+
     visit users_path
 
     users.each do |user|
       expect(page).to have_content user.name
-      expect(page).to have_content user.username
     end
   end
 end
--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe Conversation, type: :model do
   it { should belong_to :sender }
   it { should belong_to :recipient }
   it { should have_many :messages }
+
+  let!(:users) { create_list(:user, 2) }
+  let!(:conversation) { create(:conversation, sender: users.first, recipient: users.last) }
+
+  describe '.between' do
+    it 'should return a conversation between two users' do
+      expect(Conversation.between(users.first.id, users.last.id).first).to eq conversation
+    end
+  end
+
+  describe '.find_or_create' do
+    context 'conversation exists' do
+      it 'returns a conversation' do
+        expect(Conversation.find_or_create(users.first.id, users.last.id)).to eq conversation
+      end
+    end
+
+    context 'conversation not found' do
+      let!(:user1) { create(:user) }
+      let!(:user2) { create(:user) }
+
+      it 'creates new conversation for users' do
+        expect(Conversation.find_or_create(user1.id, user2.id)).to eq(user1.conversations.first)
+      end
+    end
+  end
 end
