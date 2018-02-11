28d752bfc78e76d92d6d27d7aed0b3e13f451d29
Dima <sumskoydima@gmail.com>
2018-02-11 23:52:50 +0300

    Refactor conversations#show

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,15 +1,23 @@
 class ConversationsController < ApplicationController
+  before_action :set_messages_range, only: [:show]
+
   def index
     @conversations = Conversation.for_user(current_user)
   end
 
   def show
     @conversation = Conversation.find(params[:id])
-    @message      = @conversation.messages.new
+    @messages     = @conversation.messages.size > @range ? @conversation.messages.last(@range) : @conversation.messages
   end
 
   def create
     @conversation = Conversation.find_or_create(current_user.id, params[:user_id])
     redirect_to @conversation
+  end
+
+  private
+
+  def set_messages_range
+    @range = 20
   end
 end
