03353d7bf48ebd51cca3f2aec606c87f1dada2fd
Dima <sumskoydima@gmail.com>
2018-05-20 02:39:27 +0300

    Add users#update with views refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
 
   respond_to :json
 
@@ -11,7 +11,18 @@ class UsersController < ApplicationController
     respond_with(@user)
   end
 
+  def edit
+  end
+
+  def update
+    respond_with(@user.update(user_params), flash[:notice] = "Success!", location: @user)
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :phone, :bio)
+  end
 
   def set_user
     @user = User.find(params[:id])
