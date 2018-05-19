35ebf8b63474c33a3e10e9d1462bbcc9f1eedc72
Dima <sumskoydima@gmail.com>
2018-05-19 20:15:25 +0300

    Refactor search results apeearance

--- app/indices/users_index.rb
+++ app/indices/users_index.rb
@@ -1,7 +1,6 @@
 ThinkingSphinx::Index.define :user, with: :real_time do
   indexes name,     sortable: true
   indexes username, sortable: true
-  indexes email
 
   has conversation_ids, type: :integer, multi: :true
   has created_at,       type: :timestamp
