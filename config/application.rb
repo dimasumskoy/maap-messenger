1133ee58dc1ace4b78f632233bf89e84deb7af54
Dima <sumskoydima@gmail.com>
2018-05-24 18:00:41 +0300

    Set up sidekiq with assets refactor

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Messenger
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
 
+    config.active_job.queue_adapter = :sidekiq
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
