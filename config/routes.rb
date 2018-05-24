f6e343b006762ef7274df3c13d4165df5fa59cf7
Dima <sumskoydima@gmail.com>
2018-05-25 02:17:30 +0300

    Remove sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
-require 'sidekiq/web'
-
 Rails.application.routes.draw do
-  authenticate :user do
-    mount Sidekiq::Web => '/sidekiq'
-  end
-
   devise_for :users,
     controllers: { registrations: 'users/registrations' },
     path: '/',
